7e60b0ffdd5bf4c9fd9aa1d4bfcfe7a9cae010ba
ivanfsilva <ivanfs27@gmail.com>
2020-03-04 17:21:58 -0300

    Adicionando dependencia Lombok e Classe Cliente

--- src/main/java/br/com/ivanfsilva/ecommerce/model/Produto.java
+++ src/main/java/br/com/ivanfsilva/ecommerce/model/Produto.java
@@ -1,13 +1,21 @@
 package br.com.ivanfsilva.ecommerce.model;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.math.BigDecimal;
 
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
 public class Produto {
 
     @Id
+    @EqualsAndHashCode.Include
     private Integer id;
     private String nome;
     private String descricao;
