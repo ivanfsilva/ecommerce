302cdef1e675d58cb24be95e98457415de1437d5
ivanfsilva <ivanfs27@gmail.com>
2022-04-11 20:29:26 -0300

    mapping many-to-one relationships

--- backend/src/test/java/br/com/ivanfsilva/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
@@ -1,9 +1,7 @@
 package br.com.ivanfsilva.ecommerce.relacionamentos;
 
 import br.com.ivanfsilva.ecommerce.EntityManagerTest;
-import br.com.ivanfsilva.ecommerce.model.Cliente;
-import br.com.ivanfsilva.ecommerce.model.Pedido;
-import br.com.ivanfsilva.ecommerce.model.StatusPedido;
+import br.com.ivanfsilva.ecommerce.model.*;
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -31,5 +29,34 @@ public class RelacionamentoManyToOneTest extends EntityManagerTest {
 
         Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
         Assert.assertNotNull(pedidoVerificacao.getCliente());
+    }
+
+    @Test
+    public void verificarRelacionamentoItemPedido() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+        Produto produto = entityManager.find(Produto.class, 1);
+
+        Pedido pedido = new Pedido();
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setTotal(BigDecimal.TEN);
+        pedido.setCliente(cliente);
+
+        ItemPedido itemPedido = new ItemPedido();
+        itemPedido.setPrecoProduto(produto.getPreco());
+        itemPedido.setQuantidade(1);
+        itemPedido.setPedido(pedido);
+        itemPedido.setProduto(produto);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.persist(itemPedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        ItemPedido itemPedidoVerificacao = entityManager.find(ItemPedido.class, itemPedido.getId());
+        Assert.assertNotNull(itemPedidoVerificacao.getPedido());
+        Assert.assertNotNull(itemPedidoVerificacao.getProduto());
     }
 }
