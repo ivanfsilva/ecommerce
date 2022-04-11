daa940383073d55bb31e2790f6f7f7849dc41930
ivanfsilva <ivanfs27@gmail.com>
2022-04-11 20:51:13 -0300

    mapping self-relationships with @ManyToOne and @OneToMany

---
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/relacionamentos/AutoRelacionamentoTest.java
@@ -0,0 +1,34 @@
+package br.com.ivanfsilva.ecommerce.relacionamentos;
+
+import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.model.Categoria;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AutoRelacionamentoTest extends EntityManagerTest {
+
+    @Test
+    public void verificarRelacionamento() {
+
+        Categoria categoriaPai = new Categoria();
+        categoriaPai.setNome("Eletrônicos");
+
+        Categoria categoria = new Categoria();
+        categoria.setNome("Celulares");
+        categoria.setCategoriaPai(categoriaPai);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(categoriaPai);
+        entityManager.persist(categoria);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Categoria categoriaVerificacao = entityManager.find(Categoria.class, categoria.getId());
+        Assert.assertNotNull(categoriaVerificacao.getCategoriaPai());
+
+        Categoria categoriaPaiVerificacao = entityManager.find(Categoria.class, categoriaPai.getId());
+        Assert.assertFalse(categoriaPaiVerificacao.getCategorias().isEmpty());
+    }
+
+}
