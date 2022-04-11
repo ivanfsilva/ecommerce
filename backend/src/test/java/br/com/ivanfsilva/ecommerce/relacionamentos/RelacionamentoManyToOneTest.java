697c439ed81b286dccfb7e7ecb6b3e93c2aa3756
ivanfsilva <ivanfs27@gmail.com>
2022-04-11 20:21:20 -0300

    mapping very-to-one relationships with @ManyToOne

---
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/relacionamentos/RelacionamentoManyToOneTest.java
@@ -0,0 +1,35 @@
+package br.com.ivanfsilva.ecommerce.relacionamentos;
+
+import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.model.Cliente;
+import br.com.ivanfsilva.ecommerce.model.Pedido;
+import br.com.ivanfsilva.ecommerce.model.StatusPedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+public class RelacionamentoManyToOneTest extends EntityManagerTest {
+
+    @Test
+    public void verificarRelacionamento() {
+        Cliente cliente = entityManager.find(Cliente.class, 1);
+
+        Pedido pedido = new Pedido();
+        pedido.setStatus(StatusPedido.AGUARDANDO);
+        pedido.setDataPedido(LocalDateTime.now());
+        pedido.setTotal(BigDecimal.TEN);
+
+        pedido.setCliente(cliente);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(pedido);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Pedido pedidoVerificacao = entityManager.find(Pedido.class, pedido.getId());
+        Assert.assertNotNull(pedidoVerificacao.getCliente());
+    }
+}
