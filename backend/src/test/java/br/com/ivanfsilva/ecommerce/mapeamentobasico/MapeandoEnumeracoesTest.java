47fc873efae128e47cab0261df71bf9a73b5c873
ivanfsilva <ivanfs27@gmail.com>
2022-04-11 13:13:51 -0300

    mapping enumerations with @Enumerated; MapeandoEnumeracoesTest created

---
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/mapeamentobasico/MapeandoEnumeracoesTest.java
@@ -0,0 +1,27 @@
+package br.com.ivanfsilva.ecommerce.mapeamentobasico;
+
+import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.model.Cliente;
+import br.com.ivanfsilva.ecommerce.model.SexoCliente;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class MapeandoEnumeracoesTest extends EntityManagerTest {
+
+    @Test
+    public void testarEnum() {
+        Cliente cliente = new Cliente();
+        cliente.setId(4);
+        cliente.setNome("José Mineiro");
+        cliente.setSexo(SexoCliente.MASCULINO);
+
+        entityManager.getTransaction().begin();
+        entityManager.persist(cliente);
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Cliente clienteVerificacao = entityManager.find(Cliente.class, cliente.getId());
+        Assert.assertNotNull(clienteVerificacao);
+    }
+}
