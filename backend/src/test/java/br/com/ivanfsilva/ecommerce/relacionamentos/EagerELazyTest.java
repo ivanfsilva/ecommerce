ee8d301a6f623a743c2ab0db4eaaa40c93e33770
ivanfsilva <ivanfs27@gmail.com>
2022-04-11 22:10:31 -0300

    understanding the workings of Eager and Lazy Loading

---
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/relacionamentos/EagerELazyTest.java
@@ -0,0 +1,16 @@
+package br.com.ivanfsilva.ecommerce.relacionamentos;
+
+import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.model.Pedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EagerELazyTest extends EntityManagerTest {
+
+    @Test
+    public void verficarComportamento() {
+        Pedido pedido = entityManager.find(Pedido.class, 1);
+
+//        pedido.getItens().isEmpty();
+    }
+}
