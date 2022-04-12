771311d22527214dae42321c26286b41a3eb1cb1
ivanfsilva <ivanfs27@gmail.com>
2022-04-11 22:35:33 -0300

    states and life cycle of objects

---
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/conhecendoentitymanager/EstadosECicloDeVidaTest.java
@@ -0,0 +1,23 @@
+package br.com.ivanfsilva.ecommerce.conhecendoentitymanager;
+
+import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.model.Categoria;
+import org.junit.Test;
+
+public class EstadosECicloDeVidaTest extends EntityManagerTest {
+
+    @Test
+    public void analisarEstados() {
+        Categoria categoriaNovo = new Categoria();
+
+        Categoria categoriaGerenciadaMerge = entityManager.merge(categoriaNovo);
+
+        Categoria categoriaGerenciada = entityManager.find(Categoria.class, 1);
+
+        entityManager.remove(categoriaGerenciada);
+        entityManager.persist(categoriaGerenciada);
+
+        entityManager.detach(categoriaGerenciada);
+    }
+
+}
