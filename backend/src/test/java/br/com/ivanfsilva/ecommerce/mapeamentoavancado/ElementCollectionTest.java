5cc8e4f75a86bffb73b110234f5296dd9daf1be4
ivanfsilva <ivanfs27@gmail.com>
2022-04-13 14:34:20 -0300

    mapping inline object collections with ElementCollection annotation

--- backend/src/test/java/br/com/ivanfsilva/ecommerce/mapeamentoavancado/ElementCollectionTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/mapeamentoavancado/ElementCollectionTest.java
@@ -1,6 +1,7 @@
 package br.com.ivanfsilva.ecommerce.mapeamentoavancado;
 
 import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.model.Atributo;
 import br.com.ivanfsilva.ecommerce.model.Produto;
 import org.junit.Assert;
 import org.junit.Test;
@@ -22,5 +23,20 @@ public class ElementCollectionTest extends EntityManagerTest {
 
         Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
         Assert.assertFalse(produtoVerificacao.getTags().isEmpty());
+    }
+
+    @Test
+    public void aplicarAtributos() {
+        entityManager.getTransaction().begin();
+
+        Produto produto = entityManager.find(Produto.class, 1);
+        produto.setAtributos(Arrays.asList(new Atributo("tela", "320x600")));
+
+        entityManager.getTransaction().commit();
+
+        entityManager.clear();
+
+        Produto produtoVerificacao = entityManager.find(Produto.class, produto.getId());
+        Assert.assertFalse(produtoVerificacao.getAtributos().isEmpty());
     }
 }
