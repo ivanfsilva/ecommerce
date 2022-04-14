5791bdc76566201d287c0fa7848d67628e64c8d0
ivanfsilva <ivanfs27@gmail.com>
2022-04-14 14:37:44 -0300

    reverting inheritance mapping to single table

--- backend/src/main/java/br/com/ivanfsilva/ecommerce/model/PagamentoBoleto.java
+++ backend/src/main/java/br/com/ivanfsilva/ecommerce/model/PagamentoBoleto.java
@@ -6,13 +6,11 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Getter
 @Setter
 @DiscriminatorValue("boleto")
 @Entity
-@Table(name = "pagamento_boleto")
 public class PagamentoBoleto extends Pagamento {
 
     @Column(name = "codigo_barras")
--- backend/src/main/java/br/com/ivanfsilva/ecommerce/model/PagamentoCartao.java
+++ backend/src/main/java/br/com/ivanfsilva/ecommerce/model/PagamentoCartao.java
@@ -6,13 +6,11 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
-import javax.persistence.Table;
 
 @Getter
 @Setter
 @DiscriminatorValue("cartao")
 @Entity
-@Table(name = "pagamento_cartao")
 public class PagamentoCartao extends Pagamento {
 
     @Column(name = "numero_cartao")
