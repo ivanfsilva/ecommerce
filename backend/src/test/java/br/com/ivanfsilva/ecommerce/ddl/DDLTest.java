8fa3ccc6cd62e71f50383541672da3f62007c966
ivanfsilva <ivanfs27@gmail.com>
2022-04-14 15:08:53 -0300

    setting table details with Table annotation

---
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/ddl/DDLTest.java
@@ -0,0 +1,12 @@
+package br.com.ivanfsilva.ecommerce.ddl;
+
+import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import org.junit.Test;
+
+public class DDLTest extends EntityManagerTest {
+
+    @Test
+    public void gerarDDL() {
+        // Teste vazio... Somente para exibir DDL.
+    }
+}
