fc89fb70fe6280aa962a1b13e5469600bdc205bd
ivanfsilva <ivanfs27@gmail.com>
2022-04-14 15:34:35 -0300

    cleaning imports

--- backend/src/main/java/br/com/ivanfsilva/ecommerce/model/Estoque.java
+++ backend/src/main/java/br/com/ivanfsilva/ecommerce/model/Estoque.java
@@ -1,10 +1,12 @@
 package br.com.ivanfsilva.ecommerce.model;
 
-import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Getter
 @Setter
