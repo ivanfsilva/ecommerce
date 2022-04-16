3d73dbead12369379608212a8bc982a72d6be26f
ivanfsilva <ivanfs27@gmail.com>
2022-04-16 19:14:09 -0300

    using conditional expression like

---
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -0,0 +1,24 @@
+package br.com.ivanfsilva.ecommerce.jpql;
+
+import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TemporalType;
+import javax.persistence.TypedQuery;
+import java.util.Date;
+import java.util.List;
+
+public class ExpressoesCondicionaisTest extends EntityManagerTest {
+
+    @Test
+    public void usarExpressaoCondicionalLike() {
+        String jpql = "select c from Cliente c where c.nome like concat('%', :nome, '%')";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+        typedQuery.setParameter("nome", "a");
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+}
