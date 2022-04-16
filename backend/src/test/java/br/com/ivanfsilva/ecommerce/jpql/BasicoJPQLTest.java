ab029a2a406c8b8abd43e33c8169038fe48f4278
ivanfsilva <ivanfs27@gmail.com>
2022-04-16 14:54:30 -0300

    understanding the differences between TypedQuery and Query

--- backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/BasicoJPQLTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/BasicoJPQLTest.java
@@ -5,6 +5,7 @@ import br.com.ivanfsilva.ecommerce.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
 
+import javax.persistence.Query;
 import javax.persistence.TypedQuery;
 import java.util.List;
 
@@ -21,6 +22,22 @@ public class BasicoJPQLTest extends EntityManagerTest {
         Assert.assertNotNull(pedido);
 
 //        List<Pedido> lista = typedQuery.getResultList();
+//        Assert.assertFalse(lista.isEmpty());
+    }
+
+    @Test
+    public void mostrarDiferencaQueries() {
+        String jpql = "select p from Pedido p where p.id = 1";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        Pedido pedido1 = typedQuery.getSingleResult();
+        Assert.assertNotNull(pedido1);
+
+        Query query = entityManager.createQuery(jpql);
+        Pedido pedido2 = (Pedido) query.getSingleResult();
+        Assert.assertNotNull(pedido2);
+
+//        List<Pedido> lista = query.getResultList();
 //        Assert.assertFalse(lista.isEmpty());
     }
 }
