a0265faa958d95c17285546e1aaa2a329eae8207
ivanfsilva <ivanfs27@gmail.com>
2022-04-16 18:43:50 -0300

    understanding Path Expressions

---
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/PathExpressionTest.java
@@ -0,0 +1,21 @@
+package br.com.ivanfsilva.ecommerce.jpql;
+
+import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class PathExpressionTest extends EntityManagerTest {
+
+    @Test
+    public void usarPathExpressions() {
+        String jpql = "select p.cliente.nome from Pedido p";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+}
