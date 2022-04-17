771192b7d3352cd401506003996a609ecd095eba
ivanfsilva <ivanfs27@gmail.com>
2022-04-16 23:08:33 -0300

    Using higher and smaller expressions with dates

--- backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/ExpressoesCondicionaisTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/ExpressoesCondicionaisTest.java
@@ -1,16 +1,29 @@
 package br.com.ivanfsilva.ecommerce.jpql;
 
 import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TemporalType;
 import javax.persistence.TypedQuery;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.List;
 
 public class ExpressoesCondicionaisTest extends EntityManagerTest {
+
+    @Test
+    public void usarMaiorMenorComDatas() {
+        String jpql = "select p from Pedido p where p.dataCriacao > :data";
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(jpql, Pedido.class);
+        typedQuery.setParameter("data", LocalDateTime.now().minusDays(2));
+
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
 
     @Test
     public void usarMaiorMenor() {
