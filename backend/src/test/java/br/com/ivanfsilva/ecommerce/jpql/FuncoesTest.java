a2caa290edc87b52e23bc01d32c5904fc08cf165
ivanfsilva <ivanfs27@gmail.com>
2022-04-17 12:12:45 -0300

    using functions for dates

---
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/FuncoesTest.java
@@ -0,0 +1,43 @@
+package br.com.ivanfsilva.ecommerce.jpql;
+
+import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class FuncoesTest extends EntityManagerTest {
+
+    @Test
+    public void aplicarFuncaoData() {
+        // TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
+        // current_date, current_time, current_timestamp
+        // year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao)
+
+        String jpql = "select hour(p.dataCriacao), minute(p.dataCriacao), second(p.dataCriacao) " +
+                " from Pedido p where hour(p.dataCriacao) > 00";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1] + " | " + arr[2]));
+    }
+
+    @Test
+    public void aplicarFuncaoString() {
+        // concat, length, locate, substring, lower, upper, trim
+
+        String jpql = "select c.nome, length(trim(c.nome)) from Categoria c " +
+                " where substring(c.nome, 1, 1) = 'N'";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " - " + arr[1]));
+    }
+}
