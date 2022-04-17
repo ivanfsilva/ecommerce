24e9171b5f504dd63a958a1f652f100e7f38cd74
ivanfsilva <ivanfs27@gmail.com>
2022-04-17 12:26:49 -0300

    more examples of functions for collections

--- backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/FuncoesTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/FuncoesTest.java
@@ -10,6 +10,33 @@ import java.util.List;
 public class FuncoesTest extends EntityManagerTest {
 
     @Test
+    public void totalItensDeUmPedido() {
+
+        String jpql = "select sum(ip.precoProduto * ip.quantidade) from ItemPedido ip join ip.pedido p where p.id = :pedidoId";
+
+        TypedQuery<Number> typedQuery = entityManager.createQuery(jpql, Number.class)
+                .setParameter("pedidoId", 1);
+
+        List<Number> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println(obj));
+    }
+
+    @Test
+    public void totalItensPorPedido() {
+
+        String jpql = "select p.id, sum(ip.precoProduto * ip.quantidade) from ItemPedido ip join ip.pedido p group by p.id";
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(arr[0] + " | " + arr[1]));
+    }
+
+    @Test
     public void aplicarFuncaoColecao() {
         String jpql = "select size(p.itens) from Pedido p where size(p.itens) > 1";
 
