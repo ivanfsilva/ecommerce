41927610977d7c7e55666cbfcac1bf9cdae097e1
ivanfsilva <ivanfs27@gmail.com>
2022-04-18 11:10:11 -0300

    using group by

--- backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/GroupByTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/GroupByTest.java
@@ -20,9 +20,22 @@ public class GroupByTest extends EntityManagerTest {
 //                " group by year(p.dataCriacao), month(p.dataCriacao) ";
 
 //         Total de vendas por categoria.
-        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
-                " join ip.produto pro join pro.categorias c " +
-                " group by c.id";
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.produto pro join pro.categorias c " +
+//                " group by c.id";
+
+//        Total de vendas por cliente
+//        String jpql = "select c.nome, sum(ip.precoProduto) from ItemPedido ip " +
+//                " join ip.pedido p join p.cliente c " +
+//                " group by c.id";
+
+//        Total de vendas por dia e por categoria
+        String jpql = "select " +
+                " concat(year(p.dataCriacao), '/', month(p.dataCriacao), '/', day(p.dataCriacao)), " +
+                " concat(c.nome, ': ', sum(ip.precoProduto)) " +
+                " from ItemPedido ip join ip.pedido p join ip.produto pro join pro.categorias c " +
+                " group by year(p.dataCriacao), month(p.dataCriacao), day(p.dataCriacao), c.id " +
+                " order by p.dataCriacao, c.nome ";
 
 
         TypedQuery<Object[]> typedQuery = entityManager.createQuery(jpql, Object[].class);
