6bc3c805f0751245cbd6bc7101b6f80c1ba11e58
ivanfsilva <ivanfs27@gmail.com>
2022-04-18 23:12:13 -0300

    updating objects in batch

--- backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/OperacoesEmLoteTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/OperacoesEmLoteTest.java
@@ -4,6 +4,7 @@ import br.com.ivanfsilva.ecommerce.EntityManagerTest;
 import br.com.ivanfsilva.ecommerce.model.Produto;
 import org.junit.Test;
 
+import javax.persistence.Query;
 import java.io.*;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -12,6 +13,20 @@ import java.util.stream.Collectors;
 public class OperacoesEmLoteTest extends EntityManagerTest {
 
     private static final int LIMITE_INSERCOES = 4;
+
+    @Test
+    public void atualizarEmLote() {
+        entityManager.getTransaction().begin();
+
+        String jpql = "update Produto p set p.preco = p.preco + (p.preco * 0.1) " +
+                " where exists (select 1 from p.categorias c2 where c2.id = :categoria)";
+
+        Query query = entityManager.createQuery(jpql);
+        query.setParameter("categoria", 2);
+        query.executeUpdate();
+
+        entityManager.getTransaction().commit();
+    }
 
     @Test
     public void inserirEmLote() throws IOException {
