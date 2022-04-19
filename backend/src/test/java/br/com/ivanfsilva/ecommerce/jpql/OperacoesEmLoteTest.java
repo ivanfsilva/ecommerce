8635b05fe8ae5abc8afb8c83cefd88122529a200
ivanfsilva <ivanfs27@gmail.com>
2022-04-18 23:01:21 -0300

    doing batch operations

---
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/OperacoesEmLoteTest.java
@@ -0,0 +1,53 @@
+package br.com.ivanfsilva.ecommerce.jpql;
+
+import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.model.Produto;
+import org.junit.Test;
+
+import java.io.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.stream.Collectors;
+
+public class OperacoesEmLoteTest extends EntityManagerTest {
+
+    private static final int LIMITE_INSERCOES = 4;
+
+    @Test
+    public void inserirEmLote() throws IOException {
+        InputStream in = OperacoesEmLoteTest.class.getClassLoader()
+                .getResourceAsStream("produtos/importar.txt");
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+
+        entityManager.getTransaction().begin();
+
+        int contadorInsercoes = 0;
+
+        for(String linha: reader.lines().collect(Collectors.toList())) {
+            if (linha.isBlank()) {
+                continue;
+            }
+
+            String[] produtoColuna = linha.split(";");
+            Produto produto = new Produto();
+            produto.setNome(produtoColuna[0]);
+            produto.setDescricao(produtoColuna[1]);
+            produto.setPreco(new BigDecimal(produtoColuna[2]));
+            produto.setDataCriacao(LocalDateTime.now());
+
+            entityManager.persist(produto);
+
+            if (++contadorInsercoes == LIMITE_INSERCOES) {
+                entityManager.flush();
+                entityManager.clear();
+
+                contadorInsercoes = 0;
+
+                System.out.println("---------------------------------");
+            }
+        }
+
+        entityManager.getTransaction().commit();
+    }
+}
