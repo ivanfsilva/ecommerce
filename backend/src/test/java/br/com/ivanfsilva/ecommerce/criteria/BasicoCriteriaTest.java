d2dc5d55711784f328b06dcce3cbd8c3cffc9c8a
ivanfsilva <ivanfs27@gmail.com>
2022-04-19 18:32:39 -0300

    selecting an entity attribute as query return

--- backend/src/test/java/br/com/ivanfsilva/ecommerce/criteria/BasicoCriteriaTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/criteria/BasicoCriteriaTest.java
@@ -9,8 +9,24 @@ import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
 import javax.persistence.criteria.Root;
+import java.math.BigDecimal;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void selecionarUmAtributoParaRetorno() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<BigDecimal> criteriaQuery = criteriaBuilder.createQuery(BigDecimal.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root.get("total"));
+
+        criteriaQuery.where(criteriaBuilder.equal(root.get("id"), 1));
+
+        TypedQuery<BigDecimal> typedQuery = entityManager.createQuery(criteriaQuery);
+        BigDecimal total = typedQuery.getSingleResult();
+        Assert.assertEquals(new BigDecimal("2398.00"), total);
+    }
 
     @Test
     public void buscarPorIdentificador() {
