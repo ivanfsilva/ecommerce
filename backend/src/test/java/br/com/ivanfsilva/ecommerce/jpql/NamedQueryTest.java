e060c2132bce7ea2b6e6dea6c61ee5f78e7aa0c9
ivanfsilva <ivanfs27@gmail.com>
2022-04-18 23:54:20 -0300

    externalizing queries in an XML file

--- backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/NamedQueryTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/jpql/NamedQueryTest.java
@@ -11,6 +11,17 @@ import java.util.List;
 public class NamedQueryTest extends EntityManagerTest {
 
     @Test
+    public void executarConsultaArquivoXMLEspecificoProduto() {
+        TypedQuery<Produto> typedQuery = entityManager
+                .createNamedQuery("Produto.listar", Produto.class);
+
+        List<Produto> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+    }
+
+
+    @Test
     public void executarConsulta() {
         TypedQuery<Produto> typedQuery = entityManager
                 .createNamedQuery("Produto.listarPorCategoria", Produto.class);
