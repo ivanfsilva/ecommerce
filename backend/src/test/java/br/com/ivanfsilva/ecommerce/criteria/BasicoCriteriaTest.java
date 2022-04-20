9278648301fd64c802f8db5eac3e93b4bcf6f04f
ivanfsilva <ivanfs27@gmail.com>
2022-04-20 10:58:04 -0300

    working with projections and DTO

--- backend/src/test/java/br/com/ivanfsilva/ecommerce/criteria/BasicoCriteriaTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/criteria/BasicoCriteriaTest.java
@@ -1,6 +1,7 @@
 package br.com.ivanfsilva.ecommerce.criteria;
 
 import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.dto.ProdutoDTO;
 import br.com.ivanfsilva.ecommerce.model.Pedido;
 import br.com.ivanfsilva.ecommerce.model.Produto;
 import org.junit.Assert;
@@ -15,6 +16,22 @@ import java.math.BigDecimal;
 import java.util.List;
 
 public class BasicoCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void projetarOResultadoDTO() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<ProdutoDTO> criteriaQuery = criteriaBuilder.createQuery(ProdutoDTO.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(criteriaBuilder
+                .construct(ProdutoDTO.class, root.get("id"), root.get("nome")));
+
+        TypedQuery<ProdutoDTO> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<ProdutoDTO> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(dto -> System.out.println("ID: " + dto.getId() + ", Nome: " + dto.getNome()));
+    }
 
     @Test
     public void projetarOResultadoTuple() {
