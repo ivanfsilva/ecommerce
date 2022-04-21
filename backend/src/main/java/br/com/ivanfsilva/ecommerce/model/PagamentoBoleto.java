5ab02295fb09ae057879807b07759a0b8622bb1e
ivanfsilva <ivanfs27@gmail.com>
2022-04-21 19:13:54 -0300

    using dates functions

--- backend/src/main/java/br/com/ivanfsilva/ecommerce/model/PagamentoBoleto.java
+++ backend/src/main/java/br/com/ivanfsilva/ecommerce/model/PagamentoBoleto.java
@@ -6,6 +6,7 @@ import lombok.Setter;
 import javax.persistence.Column;
 import javax.persistence.DiscriminatorValue;
 import javax.persistence.Entity;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -15,4 +16,7 @@ public class PagamentoBoleto extends Pagamento {
 
     @Column(name = "codigo_barras", length = 100)
     private String codigoBarras;
+
+    @Column(name = "data_vencimento")
+    private LocalDate dataVencimento;
 }
--- backend/src/test/java/br/com/ivanfsilva/ecommerce/criteria/FuncoesCriteriaTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/criteria/FuncoesCriteriaTest.java
@@ -1,17 +1,55 @@
 package br.com.ivanfsilva.ecommerce.criteria;
 
 import br.com.ivanfsilva.ecommerce.EntityManagerTest;
-import br.com.ivanfsilva.ecommerce.model.Cliente;
+import br.com.ivanfsilva.ecommerce.model.*;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.TypedQuery;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Join;
 import javax.persistence.criteria.Root;
 import java.util.List;
 
 public class FuncoesCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void aplicarFuncaoData() {
+        // current_date, current_time, current_timestamp
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Object[]> criteriaQuery = criteriaBuilder.createQuery(Object[].class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+        Join<Pedido, Pagamento> joinPagamento = root.join("pagamento");
+        Join<Pedido, PagamentoBoleto> joinPagamentoBoleto = criteriaBuilder
+                .treat(joinPagamento, PagamentoBoleto.class);
+
+        criteriaQuery.multiselect(
+                root.get("id"),
+                criteriaBuilder.currentDate(),
+                criteriaBuilder.currentTime(),
+                criteriaBuilder.currentTimestamp()
+        );
+
+        criteriaQuery.where(
+                criteriaBuilder.between(criteriaBuilder.currentDate(),
+                        root.get("dataCriacao").as(java.sql.Date.class),
+                        joinPagamentoBoleto.get("dataVencimento").as(java.sql.Date.class)),
+                criteriaBuilder.equal(root.get("status"), StatusPedido.AGUARDANDO)
+        );
+
+        TypedQuery<Object[]> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Object[]> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(arr -> System.out.println(
+                arr[0]
+                        + ", current_date: " + arr[1]
+                        + ", current_time: " + arr[2]
+                        + ", current_timestamp: " + arr[3]));
+    }
 
     @Test
     public void aplicarFuncaoString() {
