e60b67770f72c27be385ce9f91e47c335a791190
ivanfsilva <ivanfs27@gmail.com>
2022-04-21 15:06:21 -0300

    using conditional expression like

---
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/criteria/ExpressoesCondicionaisCriteriaTest.java
@@ -0,0 +1,30 @@
+package br.com.ivanfsilva.ecommerce.criteria;
+
+import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.model.Cliente;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class ExpressoesCondicionaisCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void usarExpressaoCondicionalLike() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Cliente> criteriaQuery = criteriaBuilder.createQuery(Cliente.class);
+        Root<Cliente> root = criteriaQuery.from(Cliente.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(criteriaBuilder.like(root.get("nome"), "%a%"));
+
+        TypedQuery<Cliente> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Cliente> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
+}
