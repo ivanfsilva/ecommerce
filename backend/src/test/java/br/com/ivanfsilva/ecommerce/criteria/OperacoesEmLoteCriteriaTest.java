5908e25c17235a20bb1ba37f92601c7408bddee2
ivanfsilva <ivanfs27@gmail.com>
2022-04-22 16:02:12 -0300

    updating objects in batch

---
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/criteria/OperacoesEmLoteCriteriaTest.java
@@ -0,0 +1,45 @@
+package br.com.ivanfsilva.ecommerce.criteria;
+
+import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.model.Categoria;
+import br.com.ivanfsilva.ecommerce.model.Produto;
+import org.junit.Test;
+
+import javax.persistence.Query;
+import javax.persistence.criteria.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class OperacoesEmLoteCriteriaTest extends EntityManagerTest {
+
+    @Test
+    public void atualizarEmLote() {
+        entityManager.getTransaction().begin();
+
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaUpdate<Produto> criteriaUpdate = criteriaBuilder.createCriteriaUpdate(Produto.class);
+        Root<Produto> root = criteriaUpdate.from(Produto.class);
+
+//        criteriaUpdate.set(root.get("preco"),
+//                criteriaBuilder.prod(root.get("preco"), "1.1"));
+
+        Subquery<Integer> subquery = criteriaUpdate.subquery(Integer.class);
+        Root<Produto> subqueryRoot = subquery.correlate(root);
+        Join<Produto, Categoria> joinCategoria = subqueryRoot.join("categorias");
+        subquery.select(criteriaBuilder.literal(1));
+        subquery.where(criteriaBuilder.equal(joinCategoria.get("id"), 2));
+
+        criteriaUpdate.where(criteriaBuilder.exists(subquery));
+
+        Query query = entityManager.createQuery(criteriaUpdate);
+        query.executeUpdate();
+
+        entityManager.getTransaction().commit();
+    }
+}
