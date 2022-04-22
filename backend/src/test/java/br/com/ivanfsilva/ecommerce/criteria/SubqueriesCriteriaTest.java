6b25d72fd8cf5a37fb4f73196562e4e2e7e17078
ivanfsilva <ivanfs27@gmail.com>
2022-04-22 14:16:51 -0300

    creating subqueries with exists

--- backend/src/test/java/br/com/ivanfsilva/ecommerce/criteria/SubqueriesCriteriaTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/criteria/SubqueriesCriteriaTest.java
@@ -13,6 +13,33 @@ import java.util.List;
 public class SubqueriesCriteriaTest extends EntityManagerTest {
 
     @Test
+    public void perquisarComExistsExercicio() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Produto> criteriaQuery = criteriaBuilder.createQuery(Produto.class);
+        Root<Produto> root = criteriaQuery.from(Produto.class);
+
+        criteriaQuery.select(root);
+
+        Subquery<Integer> subquery = criteriaQuery.subquery(Integer.class);
+        Root<ItemPedido> subqueryRoot = subquery.from(ItemPedido.class);
+        subquery.select(criteriaBuilder.literal(1));
+        subquery.where(
+                criteriaBuilder.equal(subqueryRoot.get("produto"), root),
+                criteriaBuilder.notEqual(
+                        subqueryRoot.get("precoProduto"), root.get("preco"))
+        );
+
+        criteriaQuery.where(criteriaBuilder.exists(subquery));
+
+        TypedQuery<Produto> typedQuery = entityManager.createQuery(criteriaQuery);
+
+        List<Produto> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+
+        lista.forEach(obj -> System.out.println("ID: " + obj.getId()));
+    }
+
+    @Test
     public void pesquisarComINExercicio() {
         CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
         CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
