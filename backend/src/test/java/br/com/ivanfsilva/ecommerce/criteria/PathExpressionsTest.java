cba53853882b728f9b515c05809e650636dcf1f8
ivanfsilva <ivanfs27@gmail.com>
2022-04-22 09:29:48 -0300

    difference between expressions, paths and predicates

---
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/criteria/PathExpressionsTest.java
@@ -0,0 +1,34 @@
+package br.com.ivanfsilva.ecommerce.criteria;
+
+import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.model.Pedido;
+import org.junit.Assert;
+import org.junit.Test;
+
+import javax.persistence.TypedQuery;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
+import java.util.List;
+
+public class PathExpressionsTest extends EntityManagerTest {
+
+
+
+    @Test
+    public void usarPathExpression() {
+        CriteriaBuilder criteriaBuilder = entityManager.getCriteriaBuilder();
+        CriteriaQuery<Pedido> criteriaQuery = criteriaBuilder.createQuery(Pedido.class);
+        Root<Pedido> root = criteriaQuery.from(Pedido.class);
+
+        criteriaQuery.select(root);
+
+        criteriaQuery.where(
+                criteriaBuilder.like(root.get("cliente").get("nome"), "M%"));
+
+        TypedQuery<Pedido> typedQuery = entityManager.createQuery(criteriaQuery);
+        List<Pedido> lista = typedQuery.getResultList();
+
+        Assert.assertFalse(lista.isEmpty());
+    }
+}
