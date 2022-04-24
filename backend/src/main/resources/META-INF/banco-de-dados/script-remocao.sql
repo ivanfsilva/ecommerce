8786f5bae6f5ecc07dcd503aeda412aac4e41266
ivanfsilva <ivanfs27@gmail.com>
2022-04-23 23:29:45 -0300

    running SQL and returning an entity

--- backend/src/main/resources/META-INF/banco-de-dados/script-remocao.sql
+++ backend/src/main/resources/META-INF/banco-de-dados/script-remocao.sql
@@ -2,3 +2,8 @@ drop table if exists testando;
 
 drop function if exists acima_media_faturamento;
 
+drop table if exists produto_loja;
+
+drop table if exists ecm_produto;
+
+drop table if exists erp_produto;
--- backend/src/test/java/br/com/ivanfsilva/ecommerce/consultasnativas/ConsultaNaticaTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/consultasnativas/ConsultaNaticaTest.java
@@ -1,12 +1,36 @@
 package br.com.ivanfsilva.ecommerce.consultasnativas;
 
 import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.model.Produto;
 import org.junit.Test;
 
 import javax.persistence.Query;
 import java.util.List;
 
 public class ConsultaNaticaTest extends EntityManagerTest {
+
+    @Test
+    public void executarSQLRetornandoEntidade() {
+//        String sql = "select id, nome, descricao, data_criacao, data_ultima_atualizacao, preco, foto " +
+//                "     from produto_loja";
+
+//        String sql = "select prd_id id, prd_nome nome, prd_descricao descricao, " +
+//                "            prd_data_criacao data_criacao, prd_data_ultima_atualizacao data_ultima_atualizacao, " +
+//                "            prd_preco preco, prd_foto foto " +
+//                "     from ecm_produto";
+
+        String sql = "select id, nome, descricao, " +
+                "            null data_criacao, null data_ultima_atualizacao, " +
+                "            preco, null foto " +
+                "     from erp_produto";
+
+        Query query = entityManager.createNativeQuery(sql, Produto.class);
+
+        List<Produto> lista = query.getResultList();
+
+        lista.stream().forEach(obj -> System.out.println(
+                String.format("Produto => ID: %s, Nome: %s", obj.getId(), obj.getNome())));
+    }
 
     @Test
     public void executarSQL() {
