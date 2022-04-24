0273a40d118002b2c3a50e85f60fe249f6d8646c
ivanfsilva <ivanfs27@gmail.com>
2022-04-23 23:33:53 -0300

    passing parameters to native query

--- backend/src/test/java/br/com/ivanfsilva/ecommerce/consultasnativas/ConsultaNaticaTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/consultasnativas/ConsultaNaticaTest.java
@@ -10,6 +10,22 @@ import java.util.List;
 public class ConsultaNaticaTest extends EntityManagerTest {
 
     @Test
+    public void passarParametros() {
+        String sql = "select prd_id id, prd_nome nome, prd_descricao descricao, " +
+                "            prd_data_criacao data_criacao, prd_data_ultima_atualizacao data_ultima_atualizacao, " +
+                "            prd_preco preco, prd_foto foto " +
+                " from ecm_produto where prd_id = :id";
+
+        Query query = entityManager.createNativeQuery(sql, Produto.class);
+        query.setParameter("id", 201);
+
+        List<Produto> lista = query.getResultList();
+
+        lista.stream().forEach(obj -> System.out.println(
+                String.format("Produto => ID: %s, Nome: %s", obj.getId(), obj.getNome())));
+    }
+
+    @Test
     public void executarSQLRetornandoEntidade() {
 //        String sql = "select id, nome, descricao, data_criacao, data_ultima_atualizacao, preco, foto " +
 //                "     from produto_loja";
