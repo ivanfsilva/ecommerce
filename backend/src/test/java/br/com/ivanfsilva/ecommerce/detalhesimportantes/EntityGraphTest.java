87c034eeaada919a9a54231de74b64275143aded
ivanfsilva <ivanfs27@gmail.com>
2022-04-26 11:45:30 -0300

    adding a Subgraph in the query

--- backend/src/test/java/br/com/ivanfsilva/ecommerce/detalhesimportantes/EntityGraphTest.java
+++ backend/src/test/java/br/com/ivanfsilva/ecommerce/detalhesimportantes/EntityGraphTest.java
@@ -1,17 +1,36 @@
 package br.com.ivanfsilva.ecommerce.detalhesimportantes;
 
 import br.com.ivanfsilva.ecommerce.EntityManagerTest;
+import br.com.ivanfsilva.ecommerce.model.Cliente;
 import br.com.ivanfsilva.ecommerce.model.Pedido;
 import org.junit.Assert;
 import org.junit.Test;
 
 import javax.persistence.EntityGraph;
+import javax.persistence.Subgraph;
 import javax.persistence.TypedQuery;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class EntityGraphTest extends EntityManagerTest {
+
+    @Test
+    public void buscarAtributosEssenciaisDePedido02() {
+        EntityGraph<Pedido> entityGraph = entityManager.createEntityGraph(Pedido.class);
+        entityGraph.addAttributeNodes(
+                "dataCriacao", "status", "total");
+
+        Subgraph<Cliente> subgraphCliente = entityGraph
+                .addSubgraph("cliente", Cliente.class);
+        subgraphCliente.addAttributeNodes("nome", "cpf");
+
+        TypedQuery<Pedido> typedQuery = entityManager
+                .createQuery("select p from Pedido p", Pedido.class);
+        typedQuery.setHint("javax.persistence.fetchgraph", entityGraph);
+        List<Pedido> lista = typedQuery.getResultList();
+        Assert.assertFalse(lista.isEmpty());
+    }
 
     @Test
     public void buscarAtributosEssenciaisDePedido() {
